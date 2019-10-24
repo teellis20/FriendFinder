8266e70bd1e5b7d44422ee8de11d2a47ecf50d1a
Tellis <teellis20@gmail.com>
2019-10-23 19:59:03 -0700

    html created

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,9 +4,9 @@ var friendsArr = [
         photo: "http://media.japanpowered.com/images/goku.png",
         scores: [5,5,5,5,5,5,5,5,5,5]
     }, {
-        name: "Cell",
-        photo: "http://www.google.com",
-        scores: [1,4,2,2,2,4,3,1,1,1]
+        name: "Winnie The Pooh",
+        photo: "https://upload.wikimedia.org/wikipedia/en/thumb/1/10/Winniethepooh.png/220px-Winniethepooh.png",
+        scores: [5,4,3,2,2,4,3,5,5,5]
     }
 ];
 
